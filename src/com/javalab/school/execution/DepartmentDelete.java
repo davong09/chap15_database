1803e248606ed48e127199d37b71dbf1a0a49248
davong09 <davin0910@naver.com>
2024-05-06 16:15:38 +0900

    20240506 학과 작성

---
+++ src/com/javalab/school/execution/DepartmentDelete.java
@@ -0,0 +1,74 @@
+package com.javalab.school.execution;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ * 과목 데이터를 삭제하는 클래스
+ */
+public class DepartmentDelete {
+    // 오라클 DB에 접속해서 하기 위한 정보
+    public static void main(String[] args) {
+        Connection conn = null;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            conn = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:orcl", "school", "1234");
+
+            System.out.println("DB 접속 성공");
+
+            deleteDepartment(conn, scanner);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    } // end of main
+
+    private static void deleteDepartment(Connection conn, Scanner scanner) {
+        PreparedStatement pstmt = null;
+        try {
+            System.out.println("삭제할 과목 코드를 입력하세요: ");
+            String id = scanner.nextLine();
+
+            // SQL 쿼리문 작성
+            String sql = "DELETE FROM department WHERE department_id = ?";
+
+            // PreparedStatement 생성
+            pstmt = conn.prepareStatement(sql);
+
+            // PreparedStatement에 파라미터 설정
+            pstmt.setString(1, id);
+
+            // SQL 실행
+            int rowsDeleted = pstmt.executeUpdate();
+
+            if (rowsDeleted > 0) {
+                System.out.println("과목 정보가 성공적으로 삭제되었습니다.");
+            } else {
+                System.out.println("해당 코드의 과목을 찾을 수 없습니다.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
---
+++ src/com/javalab/school/execution/DepartmentUpdate.java
@@ -0,0 +1,78 @@
+package com.javalab.school.execution;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ * 과목 데이터를 수정하는 클래스
+ */
+public class DepartmentUpdate {
+    // 오라클 DB에 접속해서 하기 위한 정보
+    public static void main(String[] args) {
+        Connection conn = null;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            conn = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:orcl", "school", "1234");
+
+            System.out.println("DB 접속 성공");
+
+            updateDepartment(conn, scanner);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    } // end of main
+
+    private static void updateDepartment(Connection conn, Scanner scanner) {
+        System.out.println("수정할 과목의 코드를 입력하세요: ");
+        int id = scanner.nextInt();
+        // 사용자로부터 입력 받기
+        System.out.print("새 학과명: ");
+        String name = scanner.nextLine();
+        System.out.print("새 학과 사무실: ");
+        String office = scanner.nextLine();
+
+        PreparedStatement pstmt = null;
+
+        try {
+            // SQL 쿼리문 작성
+            String sql = "UPDATE department SET name = ?, office = ?";
+            pstmt = conn.prepareStatement(sql);
+
+            // PreparedStatement에 파라미터 설정
+            pstmt.setString(1, name);
+            pstmt.setString(2, office);
+
+            // SQL 실행
+            int rowsUpdated = pstmt.executeUpdate();
+
+            if (rowsUpdated > 0) {
+                System.out.println("과목 정보가 성공적으로 업데이트 되었습니다.");
+            } else {
+                System.out.println("해당 코드의 과목을 찾을 수 없습니다.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
