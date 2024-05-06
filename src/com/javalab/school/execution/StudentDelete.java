ac85ce7673b8d5657eca438b5c354b2941bd587a
davong09 <davin0910@naver.com>
2024-05-06 16:21:02 +0900

    색깔 입힌거랑 주석!

--- src/com/javalab/school/execution/StudentDelete.java
+++ src/com/javalab/school/execution/StudentDelete.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 /**
  * 학생 데이터를 삭제하는 클래스
  */
-public class StudentDelete {
+public class StudentDelete { // 클래스 이름 맞추고 ex) 교수면 ProfessorDelete
     // 오라클 DB에 접속해서 하기 위한 정보
     public static void main(String[] args) {
         Connection conn = null;
@@ -21,7 +21,7 @@ public class StudentDelete {
 
             System.out.println("DB 접속 성공");
 
-            deleteStudent(conn, scanner);
+            deleteStudent(conn, scanner); // 메소드 이름도 바꿔줘야함 ex) 교수면 deleteProfessor
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
@@ -35,28 +35,28 @@ public class StudentDelete {
         }
     } // end of main
 
-    private static void deleteStudent(Connection conn, Scanner scanner) {
+    private static void deleteStudent(Connection conn, Scanner scanner) { // 24줄 메소드 이름으로 수정
         PreparedStatement pstmt = null;
         try {
-            System.out.println("삭제할 학생의 ID를 입력하세요: ");
+            System.out.println("삭제할 학생의 ID를 입력하세요: "); // 여기도 출력문 바꿔주면 좋고
             String id = scanner.nextLine();
 
             // SQL 쿼리문 작성
-            String sql = "DELETE FROM student WHERE student_id = ?";
+            String sql = "DELETE FROM student WHERE student_id = ?"; // 교수 테이블의 교수 ID를 찾아야 하니까 이 부분도 수정
 
             // PreparedStatement 생성
             pstmt = conn.prepareStatement(sql);
 
             // PreparedStatement에 파라미터 설정
-            pstmt.setString(1, id);
+            pstmt.setString(1, id); // 42줄에서 여기 작성된 코드로 보면 삭제할 학생의 ID 입력한 걸 id에 저장 했으므로 여기 코드도 1, id로 나오는거라 위에서 수정 했으면 여기도 수정 해야함
 
             // SQL 실행
             int rowsDeleted = pstmt.executeUpdate();
 
             if (rowsDeleted > 0) {
-                System.out.println("학생 정보가 성공적으로 삭제되었습니다.");
+                System.out.println("학생 정보가 성공적으로 삭제되었습니다."); // 여기도 출력문 교수에 맞춰서 수정하면 좋고
             } else {
-                System.out.println("해당 ID의 학생을 찾을 수 없습니다.");
+                System.out.println("해당 ID의 학생을 찾을 수 없습니다."); // 요짝도
             }
         } catch (SQLException e) {
             e.printStackTrace();
